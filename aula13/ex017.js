9d1c12cc5822b160d546bd55a3877fd0afbc3635
Miguel Fonseca <139994589+miguelcfonseca@users.noreply.github.com>
2024-03-27 19:25:42 +0000

    loop statements

---
+++ aula13/ex017.js
@@ -0,0 +1,26 @@
+var c = 1
+
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO:
+
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/* SEM ESTRUTURA DE REPETIÇÃO (NÃO FAZER!)
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
